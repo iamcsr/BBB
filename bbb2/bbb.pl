ebaa132ae58c2730e8449ef178713542498c5dbf
iamcsr <codyschuyler@gmail.com>
2013-01-25 20:05:28 -0500

    fix test

--- bbb2/bbb.pl
+++ bbb2/bbb.pl
@@ -117,53 +117,53 @@ sub getMntDr {
 	return @rfiles;
 }
 
-sub testList {
-	my $win = Gtk2::Window->new('toplevel');
-	$win->signal_connect( delete_event => sub { Gtk2->main_quit } );
-
-	my $sw = Gtk2::ScrolledWindow->new( undef, undef );
-	$sw->set_shadow_type('etched-out');
-	$sw->set_policy( 'automatic', 'automatic' );
-
-	#This is a method of the Gtk2::Widget class,it will force a minimum
-	#size on the widget. Handy to give intitial size to a
-	#Gtk2::ScrolledWindow class object
-	$sw->set_size_request( 350, 300 );
-
-	#method of Gtk2::Container
-	$sw->set_border_width(5);
-	my $ls =
-	  Gtk2::ListStore->new( qw/Glib::String/, qw/Glib::String/,
-		qw/Glib::String/ );
-	for my $i (0 .. $#mfs){
-		my $itr = $ls->append();
-		$ls->set($itr,0,$mfs[$i]{'mp'});
-		$ls->set($itr,1,$mfs[$i]{'format'});
-		$ls->set($itr,2,$mfs[$i]{'dev'});
-		print $mfs[$i]{'mp'};
-	}
-	my $tr_view = Gtk2::TreeView->new($ls);
-	my $tree_column = Gtk2::TreeViewColumn->new();
-	my $tree_column2 = Gtk2::TreeViewColumn->new();
-	my $tree_column3 = Gtk2::TreeViewColumn->new();
-	$tree_column->set_title ("Mount Point");
-	my $mren = Gtk2::CellRendererText->new;
-	$tree_column->pack_start ($mren, FALSE);
-	$tree_column->add_attribute($mren, text => 0);
-	$tree_column2->set_title ("Format");
-	my $fren = Gtk2::CellRendererText->new;
-	$tree_column2->pack_start ($fren, FALSE);
-	$tree_column2->add_attribute($fren, text => 1);
-	my $dren = Gtk2::CellRendererText->new;
-	$tree_column3->set_title ("Device");
-	$tree_column3->pack_start ($dren, FALSE);
-	$tree_column3->add_attribute($dren, text => 2);
-	$tr_view->append_column ($tree_column);
-	$tr_view->append_column ($tree_column2);
-	$tr_view->append_column ($tree_column3);
-
-	$sw->add($tr_view);
-	$sw->show_all();
-	$win->add($sw);
-	return $win;
-}
+#sub testList {
+#	my $win = Gtk2::Window->new('toplevel');
+#	$win->signal_connect( delete_event => sub { Gtk2->main_quit } );
+#
+#	my $sw = Gtk2::ScrolledWindow->new( undef, undef );
+#	$sw->set_shadow_type('etched-out');
+#	$sw->set_policy( 'automatic', 'automatic' );
+#
+#	#This is a method of the Gtk2::Widget class,it will force a minimum
+#	#size on the widget. Handy to give intitial size to a
+#	#Gtk2::ScrolledWindow class object
+#	$sw->set_size_request( 350, 300 );
+#
+#	#method of Gtk2::Container
+#	$sw->set_border_width(5);
+#	my $ls =
+#	  Gtk2::ListStore->new( qw/Glib::String/, qw/Glib::String/,
+#		qw/Glib::String/ );
+#	for my $i (0 .. $#mfs){
+#		my $itr = $ls->append();
+#		$ls->set($itr,0,$mfs[$i]{'mp'});
+#		$ls->set($itr,1,$mfs[$i]{'format'});
+#		$ls->set($itr,2,$mfs[$i]{'dev'});
+#		print $mfs[$i]{'mp'};
+#	}
+#	my $tr_view = Gtk2::TreeView->new($ls);
+#	my $tree_column = Gtk2::TreeViewColumn->new();
+#	my $tree_column2 = Gtk2::TreeViewColumn->new();
+#	my $tree_column3 = Gtk2::TreeViewColumn->new();
+#	$tree_column->set_title ("Mount Point");
+#	my $mren = Gtk2::CellRendererText->new;
+#	$tree_column->pack_start ($mren, FALSE);
+#	$tree_column->add_attribute($mren, text => 0);
+#	$tree_column2->set_title ("Format");
+#	my $fren = Gtk2::CellRendererText->new;
+#	$tree_column2->pack_start ($fren, FALSE);
+#	$tree_column2->add_attribute($fren, text => 1);
+#	my $dren = Gtk2::CellRendererText->new;
+#	$tree_column3->set_title ("Device");
+#	$tree_column3->pack_start ($dren, FALSE);
+#	$tree_column3->add_attribute($dren, text => 2);
+#	$tr_view->append_column ($tree_column);
+#	$tr_view->append_column ($tree_column2);
+#	$tr_view->append_column ($tree_column3);
+#
+#	$sw->add($tr_view);
+#	$sw->show_all();
+#	$win->add($sw);
+#	return $win;
+#}
